811c14e412c3dc22ed5acd651e76a4ba90da43cd
tschmidt3 <tmschmidt527@gmail.com>
2021-02-17 22:26:44 -0500

    parametrize test cases

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,8 +1,7 @@
 from python_package.simple import simple_add
+import pytest
 
-def test_simple_add():
-    a = 1
-    b = 2
-    actual = simple_add(a,b)
-    expected = 3
-    assert actual == expected
+
+@pytest.mark.parametrize("a,b, expected", [(1, 2, 3), (-1, -2, -3)])
+def test_simple_add(a, b, expected):
+    assert simple_add(a, b) == expected
